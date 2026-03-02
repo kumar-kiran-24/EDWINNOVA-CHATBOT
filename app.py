0d484c60c5faa9517011b5501dc843a9c820033a
kumar-kiran-24 <kirankumar26650@gmail.com>
2026-03-02 12:12:53 +0530

    update CORS details.

--- app.py
+++ app.py
@@ -1,35 +1,53 @@
 import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.data_retriver import Dataretriver
 from src.chatbot import ChatBot
 
+app = FastAPI()
 
-app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    ] , 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class ChatRequest(BaseModel):
-    question:str
-    session_id:str
+    question: str
+    session_id: str
 
-
-chatbot=ChatBot()
-data_retriver=Dataretriver()
+chatbot = ChatBot()
+data_retriver = Dataretriver()
 
 @app.post("/chat")
-async def chat(request:ChatRequest):
+async def chat(request: ChatRequest):
     try:
-        question=request.question
-        session_id=request.session_id
-        context=data_retriver.search_data(query=question)
-        resopnse=chatbot.chatbot(question=question,context=context,session_id=session_id)
-        print(resopnse)
+        question = request.question
+        session_id = request.session_id
 
-        return {"Response":resopnse}
+        context = data_retriver.search_data(query=question)
+
+        response = chatbot.chatbot(
+            question=question,
+            context=context,
+            session_id=session_id
+        )
+
+        return {"Response": response}
+
     except Exception as e:
-        return {"error":str(e)}
+        return {"error": str(e)}
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=10000)
+    uvicorn.run("app:app", host="0.0.0.0", port=10000)
